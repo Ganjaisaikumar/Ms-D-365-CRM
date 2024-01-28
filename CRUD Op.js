390a1761914b4239a9ea2cc8c09f14f148fca8f8
sai kumar <sairaina18@gmail.com>
2024-01-28 11:52:47 -0600

    Updated function updatedEmployee

--- CRUD Op.js
+++ CRUD Op.js
@@ -26,9 +26,28 @@ function onCreateEmployee(executionContext) {
 // function to update employee
 function updateEmployee(executionContext) {
     var formContext = executionContext.getFormContext();
-    var emp_name = formContext.getAttribute("new_createemployee").getValue();
-    var guid_parent = formContext.data.entity.getId();
-    // alert("before "+guid_parent);
-    guid_parent = guid_parent.replace("{", " ").replace("}", " ");
+    var guid = formContext.data.entity.getId();
+    guid = guid.replace("{", "").replace("}", "");
+
+    //Condition to check Whether Reset field exists and reset value is true.
+    if(formContext.getAttribute("new_reset") != null && formContext.getAttribute("new_reset").getValue() === true) {
+        var entity = {};
+
+        //Assigning the "guid: of company name "vin".
+        entity["new_CompanyName@odata.bind"] = "/new_companies(6cf8d8ac-e4b0-ee11-a569-00224803b246)";
+        entity.new_isfresher = true;
+
+
+        //Pass the guid(guid of the record which can retrieved from UI url once you save the record!!)
+        Xrm.WebApi.online.updateRecord("new_employee",guid, entity).then(
+            function success(result) {
+                var updatedEntityId = result.id;
+            },
+            function (error) {
+                Xrm.Utility.alertDialog(error.message);
+            }
+        );
+    }
 }
+
 
